c4eca9994486227ab5b60c9b620ed4c751582038
Rusius02 <93829224+Rusius02@users.noreply.github.com>
2025-01-22 17:10:24 +0100

    Add address to InputDtoUser

--- Application/UseCases/Users/Dtos/OutputDtoUser.cs
+++ Application/UseCases/Users/Dtos/OutputDtoUser.cs
@@ -10,5 +10,12 @@
         public string mail { get; set; }
         public string BirthDate { get; set; }
         public string role { get; set; }
+        public string AddressStreet { get; set; }
+
+        public string AddressNumber { get; set; }
+
+        public string AddressCity { get; set; }
+        public string AddressZip { get; set; }
+        public string AddressCountry { get; set; }
     }
 }
