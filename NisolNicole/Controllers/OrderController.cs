26110d5b0ddbfe8ebe8a55a7da263cd81b0189f7
Rusius02 <93829224+Rusius02@users.noreply.github.com>
2025-07-16 16:56:59 +0200

    Implement the create in OrderController.cs because my behavior may change

--- NisolNicole/Controllers/OrderController.cs
+++ NisolNicole/Controllers/OrderController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.UseCases.Orders;
+using Application.UseCases.Orders.Dtos;
+using Microsoft.AspNetCore.Mvc;
 
 namespace NisolNicole.Controllers
 {
@@ -6,6 +8,18 @@ namespace NisolNicole.Controllers
     [Route("api/Orders")]
     public class OrderController
     {
+        private readonly UsecaseCreateOrder _usecaseCreateOrder;
 
+        public OrderController(UsecaseCreateOrder usecaseCreateOrder)
+        {
+            _usecaseCreateOrder = usecaseCreateOrder;
+        }
+        [HttpPost]
+        [Route("Create")]
+        public ActionResult<OutputDtoCreateOrder> CreateOrder([FromBody] InputDtoCreateOrder dto)
+        {
+            dto.PaymentStatus = "Order_pending";
+            return _usecaseCreateOrder.Execute(dto);
+        }
     }
 }
